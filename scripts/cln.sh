e08f384c744976235ed40397c8eb940188ef88bc
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-12-02 20:57:35 +0100

    Add system-clean-script

---
+++ scripts/cln.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+###
+# Clean the system.
+###
+
+echo "Remove Orphans..."
+sudo pacman -Rns "$(pacman -Qdtq)"
+
+echo -e "\nClean Pacman-Cache..."
+sudo pacman -Sc
+
+echo -e "\nClean Conda-Cache"
+conda clean -a
+
+echo -e "\nClean Home-Folder manually!"
+
+# TODO check rmlint (broken symlinks, duplicates, empty files/dirs, etc.)
+# TODO disk-analyzer
