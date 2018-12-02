c3fd4ecede8f4f7e0151e41de7ad2cd4ae943728
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-12-02 12:47:42 +0100

    Implement install-script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCR_DIR="$(pwd)"
+BIN_DIR=~/bin
+mkdir $BIN_DIR
+
+install () {
+    TARGET=$1
+    ln -fs $SCR_DIR/scripts/$TARGET.sh $BIN_DIR/$TARGET &&
+    echo Installed: $TARGET
+}
+
+install err
+install scr
