eb6c7ef9783e4b4da5b75db8dcf946d2df14762f
astier <aleksandrs.stier@uni-bielefeld.de>
2019-02-23 20:54:35 +0100

    Tweak statusbar

--- scripts/statusbar.sh
+++ scripts/statusbar.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-###
-# Writes to WM_NAME of X11 to generate dwms' statusbar.
-###
+##############################################################
+# Updates dwms' satusbar by writing to the WN_NAME variable. #
+##############################################################
+
+dat () {
+	echo " $(date +%H:%M)"
+}
 
 bat () {
 	BAT=$(cat /sys/class/power_supply/BAT0/capacity)
-	SYMBOL=""
 	if [ "$BAT" -lt 15 ]; then
 		SYMBOL=""
-	elif [ "$BAT" -lt 35 ]; then
+	elif [ "$BAT" -lt 40 ]; then
 		SYMBOL=""
-	elif [ "$BAT" -lt 65 ]; then
+	elif [ "$BAT" -lt 60 ]; then
 		SYMBOL=""
 	elif [ "$BAT" -lt 85 ]; then
 		SYMBOL=""
@@ -21,9 +24,4 @@ bat () {
 	echo "$SYMBOL $BAT%"
 }
 
-dat () {
-	DATE=$(date +%H:%M)
-	echo " $DATE"
-}
-
-xsetroot -name " $(bat)  $(dat)"
+xsetroot -name "$(bat)  $(dat)"
