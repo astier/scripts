51b90bcf855948f99d5b5bceabe5225a6b4c0ba8
aleks <aleks.stier@icloud.com>
2019-03-01 00:05:34 +0100

    Replace hsetroot with feh

--- scripts/wps.sh
+++ scripts/wps.sh
@@ -4,6 +4,6 @@
 
 [[ "$1" == "" ]] && echo "Specify an image." && exit
 [[ ! -f "$1" ]] && echo "File does not exist." && exit
-hsetroot -fill "$1"
+feh --bg-fill "$1"
 FILE_PATH=$(readlink -f "$1")
 echo "$FILE_PATH" > ~/.wallpaper
