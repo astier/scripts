bc8ed52214761ff1b7811ac87b9591a8cea7af57
aleks <aleks.stier@icloud.com>
2019-03-02 20:21:13 +0100

    Fix sxrap-nautilus-bug

--- scripts/sxrap.sh
+++ scripts/sxrap.sh
@@ -4,7 +4,8 @@
 # and beeing able to navigate to other images in the same CWD
 
 [[ -d "$1" ]] && sxiv -bt "$1" && exit
+img_path=$(realpath "$1")
+img_dir=$(dirname "$img_path")
 img_name=$(basename "$1")
-img_dir=$(pwd "$1")
 img_no=$(ls "$img_dir" | grep -n "$img_name" | cut -d":" -f1)
 sxiv -bn"$img_no" "$img_dir" && exit
