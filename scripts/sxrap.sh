92d53c0263548103eecec1ce4c0ed6586336bbc4
aleks <aleks.stier@icloud.com>
2019-03-03 00:23:17 +0100

    Use if-else-statements and fix no-args-bug in sxrap

--- scripts/sxrap.sh
+++ scripts/sxrap.sh
@@ -3,9 +3,14 @@
 # sxiv-wrapper as a workaround for sxivs' disability to open an image
 # and beeing able to navigate to other images in the same CWD
 
-[[ -d "$1" ]] && sxiv -bt "$1" && exit
-img_path=$(realpath "$1")
-img_dir=$(dirname "$img_path")
-img_name=$(basename "$1")
-img_no=$(ls "$img_dir" | grep -n "$img_name" | cut -d":" -f1)
-sxiv -bn"$img_no" "$img_dir" && exit
+if [[ "$1" == "" ]]; then
+	sxiv
+elif [[ -d "$1" ]]; then
+	sxiv -bt "$1"
+else
+	img_path=$(realpath "$1")
+	img_dir=$(dirname "$img_path")
+	img_name=$(basename "$1")
+	img_no=$(ls "$img_dir" | grep -n "$img_name" | cut -d":" -f1)
+	sxiv -bn "$img_no" "$img_dir"
+fi
