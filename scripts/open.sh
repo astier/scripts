adf20d3e907df6185a3b8728b568c39b2a16fd83
aleks <aleks.stier@icloud.com>
2019-03-04 13:56:50 +0100

    Tweak

--- scripts/open.sh
+++ scripts/open.sh
@@ -2,28 +2,32 @@
 
 # Opens files with different programs based on theit mime-types
 
-[[ "$1" == "" ]] && sxrap . && exit
-mime=$(file -bL --mime-type "$1")
+if [ "$1" == "" ]; then
+	sxrap .
+	exit
+fi
 
-prefix=$(echo "$mime" | cut -d"/" -f1)
+mime=$(file -bL --mime-type "$1")
+prefix=$(echo "$mime" | cut -d/ -f1)
 case "$prefix" in
 	"text")
 		# Check if it is a url-desktop-file
 		if [[ "$1" =~ \.desktop$ ]]; then
 			url=$(grep URL= < "$1")
-			[[ "$url" ]] && echo "$url" | cut -d"=" -f2 | xargs "$BROWSER" && exit
+			[[ "$url" ]] && echo "$url" | cut -d= -f2 | xargs "$BROWSER" && exit
 		fi
-		nvim "$1" && exit ;;
-	"image") sxrap "$1" && exit ;;
+		nvim "$1"
+		exit ;;
+	"image")
+		sxrap "$1"
+		exit ;;
 esac
 
-suffix=$(echo "$mime" | cut -d"/" -f2)
-case "$suffix" in
-	"pdf") zathura "$1" && exit
-esac
-
-if [ -d "$1" ]; then
+suffix=$(echo "$mime" | cut -d/ -f2)
+if [ "$suffix" == "pdf" ]; then
+	zathura "$1"
+elif [ -d "$1" ]; then
 	nautilus "$1"
 else
-	nvim "$1"
+	echo "Application for filetype $mime not specified."
 fi
