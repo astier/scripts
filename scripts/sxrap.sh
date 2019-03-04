8157827063d7092c8a729411956bfb9e21b66463
aleks <aleks.stier@icloud.com>
2019-03-04 10:13:08 +0100

    Deal with some special-cases

--- scripts/sxrap.sh
+++ scripts/sxrap.sh
@@ -3,14 +3,21 @@
 # sxiv-wrapper as a workaround for sxivs' disability to open an image
 # and beeing able to navigate to other images in the same CWD
 
-if [[ "$1" == "" ]]; then
+if [ "$1" == "" ]; then
 	sxiv
-elif [[ -d "$1" ]]; then
+elif [ -d "$1" ]; then
 	sxiv -bt "$1"
+elif [ ! -f "$1" ]; then
+	sxiv "$1"
 else
 	img_path=$(realpath "$1")
 	img_dir=$(dirname "$img_path")
 	img_name=$(basename "$1")
 	img_no=$(ls "$img_dir" | grep -n "$img_name" | cut -d":" -f1)
-	sxiv -bn "$img_no" "$img_dir"
+	lines=$(echo "$img_no" | wc -l)
+	if [ "$lines" -gt 1 ]; then
+		sxiv "$1"
+	else
+		sxiv -bn "$img_no" "$img_dir"
+	fi
 fi
