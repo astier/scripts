a4a73188392bbce015364ae60f7b204ec8dd5b4c
aleks <aleks.stier@icloud.com>
2019-04-10 21:37:05 +0200

    Clean b.sh

--- scripts/b.sh
+++ scripts/b.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-WP_CONF="$HOME/.local/share/wallpaper"
+BUFFER="$HOME/.local/share/wallpaper"
 
 set_wp() {
 	feh --bg-fill "$1"
-	realpath "$1" > "$WP_CONF"
+	realpath "$1" > "$BUFFER"
 }
 
 get_random_wp() {
 	WP=$(find "$1" -type f | grep -i -e .jpg -e .jpeg -e .png | shuf -n1)
 	if [ -z "$WP" ]; then
-		echo "No images found in provided directory." >&2
+		echo No images found in provided directory. >&2
 		exit 1
 	fi
 	realpath "$WP"
@@ -25,12 +25,12 @@ set_random_wp() {
 }
 
 delete_wp() {
-	WP_OLD=$(cat "$WP_CONF")
+	WP_OLD=$(cat "$BUFFER")
 	if file -b --mime-type "$WP_OLD" | grep -q image; then
 		set_random_wp "$(dirname "$WP_OLD")"
 		rm "$WP_OLD"
 	else
-		echo "Config-File is corrupt: $WP_OLD" >&2
+		echo Wallpaper-Path is corrupt: "$WP_OLD" >&2
 		exit 1
 	fi
 }
@@ -39,12 +39,12 @@ loop() {
 	set_random_wp "$1"
 	sleep "$2"
 	while true; do
-		set_random_wp "$(dirname "$(cat "$WP_CONF")")"
+		set_random_wp "$(dirname "$(cat "$BUFFER")")"
 		sleep "$2"
 	done
 }
 
-# Process arguments
+
 if [ "$#" == 0 ]; then
 	set_random_wp .
 elif [[ "$1" = -* ]]; then
@@ -52,13 +52,13 @@ elif [[ "$1" = -* ]]; then
 		if find "$2" -type f | grep -iq -e .jpg -e .jpeg -e .png; then
 			loop "$2" "$3"
 		else
-			echo "No images found in provided directory." >&2
+			echo No images found in provided directory. >&2
 			exit 1
 		fi
 	elif [ "$1" == "-d" ]; then
 		delete_wp
 	else
-		echo "Arguments are not valid." >&2
+		echo Bad arguments. >&2
 		exit 1
 	fi
 elif [ -d "$1" ]; then
@@ -66,6 +66,6 @@ elif [ -d "$1" ]; then
 elif file -b --mime-type "$1" | grep -q image; then
 	set_wp "$1"
 else
-	echo "Arguments are not valid." >&2
+	echo Bad arguments. >&2
 	exit 1
 fi
