c089c1e28f515ea883db1c9bc2e3bcb403f46de7
aleks <aleks.stier@icloud.com>
2019-04-10 14:37:18 +0200

    Implement bgm.loop

--- scripts/bgm.sh
+++ scripts/bgm.sh
@@ -29,14 +29,38 @@ delete_wp() {
 	fi
 }
 
-if [ "$1" == "-d" ]; then
-	delete_wp
+loop() {
+	WP=$(get_random_wp "$1") &&
+	set_wp "$WP"
+	sleep "$2"
+	while true; do
+		WP_DIR=$(dirname "$(cat "$WP_CONF")")
+		WP=$(get_random_wp "$WP_DIR") &&
+		set_wp "$WP"
+		sleep "$2"
+	done
+}
+
+if [[ "$1" = -* ]]; then
+	if [ "$1" == "-l" ] && [ -d "$2" ] && [[ "$3" =~ ^[0-9]+$ ]]; then
+		if find "$2" -type f | grep -iq -e .jpg -e .jpeg -e .png; then
+			loop "$2" "$3"
+		else
+			echo "No images found in provided directory." >&2
+			exit 1
+		fi
+	elif [ "$1" == "-d" ]; then
+		delete_wp
+	else
+		echo "Arguments are not valid." >&2
+		exit 1
+	fi
 elif [ -d "$1" ]; then
 	WP=$(get_random_wp "$1") &&
 	set_wp "$WP"
 elif file -b --mime-type "$1" | grep -q image; then
 	set_wp "$1"
 else
-	echo "Argument is not valid." >&2
+	echo "Arguments are not valid." >&2
 	exit 1
 fi
