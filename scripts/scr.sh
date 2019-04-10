7d8164b5abaefbc6cb479c99d2175d3f3b590fc7
aleks <aleks.stier@icloud.com>
2019-04-10 21:25:47 +0200

    Tweak scr.sh

--- scripts/scr.sh
+++ scripts/scr.sh
@@ -2,17 +2,15 @@
 
 # Script to generate scripts.
 
-script_name=$1
-script_path=~/Projects/scripts/scripts/$script_name.sh
+SCRIPT_NAME=$1
+SCRIPT_PATH=~/Projects/scripts/scripts/$SCRIPT_NAME.sh
 
-if [ -f "$script_path" ]; then
-	echo "ERROR: Script $script_path already exists."
-elif [ "$script_name" == "" ]; then
-	echo "Please specify a sctipt-name."
-else
-	echo "#!/usr/bin/env bash" > "$script_path"
-	echo -e "\n#" >> "$script_path"
-	chmod u+x "$script_path"
-	ln -sf "$script_path" ~/.local/bin/"$script_name"
-	$EDITOR "$script_path"
+if [ -z "$SCRIPT_NAME" ]; then
+	echo "Please specify a script-name." >&2
+	exit 1
 fi
+
+echo "#!/usr/bin/env bash" > "$SCRIPT_PATH"
+chmod u+x "$SCRIPT_PATH"
+ln -fs "$SCRIPT_PATH" ~/.local/bin/"$SCRIPT_NAME"
+$EDITOR "$SCRIPT_PATH"
