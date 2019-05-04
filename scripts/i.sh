9034a771dbc44755fd9e48e388f8743b886a9076
aleks <aleks.stier@icloud.com>
2019-05-04 13:18:08 +0200

    Tweak i

--- scripts/i.sh
+++ scripts/i.sh
@@ -5,4 +5,4 @@ date_time=$(date "+%a %d.%m.%Y %R")
 brightness=$(light | cut -d. -f1)
 volume=$(pulsemixer --get-volume | cut -d" " -f1)
 mute=$(pulsemixer --get-mute)
-echo "MUTED $mute | VOL $volume | LIGHT $brightness | BAT $battery | $date_time"
+echo "$date_time | BAT $battery | MUTED $mute | VOL $volume | LIGHT $brightness"
