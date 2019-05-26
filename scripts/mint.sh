743968752b97f1edf998284f06833c785a5affc9
Aleksandrs Stier <36510152+astier@users.noreply.github.com>
2019-05-26 21:46:11 +0200

    Add mint

---
+++ scripts/mint.sh
@@ -0,0 +1,1 @@
+apt purge --auto-remove thunderbird hexchat-common libreoffice-common gnome-calendar gnome-calculator onboard-common rhythmbox-data xplayer-common transmission-common pix-data pix-data simple-scan
