3cd48810c07eac3460998a7d380c0d2e3eb84b7d
Aleksandrs Stier <36510152+astier@users.noreply.github.com>
2019-05-26 21:59:32 +0200

    Update mint

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -1,1 +1,7 @@
 apt purge --auto-remove thunderbird hexchat-common libreoffice-common gnome-calendar gnome-calculator onboard-common rhythmbox-data xplayer-common transmission-common pix-data pix-data simple-scan
+
+apt install arc-theme dropbox
+
+sudo add-apt-repository ppa:daniruiz/flat-remix
+sudo apt-get update
+sudo apt-get install flat-remix
