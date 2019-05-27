1bb6e1cf5e67a3820208e9b8e7f9d5f266e3c555
Aleksandrs Stier <36510152+astier@users.noreply.github.com>
2019-05-27 09:10:18 +0200

    Update mint.sh

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -1,7 +1,7 @@
-apt purge --auto-remove thunderbird hexchat-common libreoffice-common gnome-calendar gnome-calculator onboard-common rhythmbox-data xplayer-common transmission-common pix-data pix-data simple-scan
-
-apt install arc-theme dropbox
-
 sudo add-apt-repository ppa:daniruiz/flat-remix
-sudo apt-get update
-sudo apt-get install flat-remix
+sudo add-apt-repository ppa:neovim-ppa/stable
+apt update
+
+apt install arc-theme dropbox flat-remix git neovim evince
+
+apt purge --auto-remove thunderbird hexchat-common libreoffice-common gnome-calendar gnome-calculator onboard-common rhythmbox-data xplayer-common transmission-common pix-data pix-data simple-scan vim-common ed nano flatpak gucharmap gnome-disk-utility xreader-common redshift xviewer gnome-font-viewer seahorse xed-common tomboy baobab gnome-logs gnome-power-manager
