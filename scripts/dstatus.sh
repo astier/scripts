7c89cbe708513cabf17766ef2b2c714bad29b371
aleks <aleks.stier@icloud.com>
2019-06-19 15:47:10 +0200

    Add dstatus

---
+++ scripts/dstatus.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+bat() {
+	bat_dir=/sys/class/power_supply/BAT0
+	[ ! -d "$bat_dir" ] && return
+	bat_cap=$(cat $bat_dir/capacity)
+	if [ "$(cat /sys/class/power_supply/BAT0/status)" = "Charging" ]; then
+		icon=""
+	elif [ "$bat_cap" -eq 10 ]; then
+		icon=""
+		notify-send -u critical "BATTERY LOW!"
+	elif [ "$bat_cap" -lt 15 ]; then
+		icon=""
+	elif [ "$bat_cap" -lt 40 ]; then
+		icon=""
+	elif [ "$bat_cap" -lt 60 ]; then
+		icon=""
+	elif [ "$bat_cap" -lt 85 ]; then
+		icon=""
+	else
+		icon=""
+	fi
+	echo "$icon $bat_cap%"
+}
+
+dat() { echo " $(date +%H:%M)"; }
+
+drb() { pgrep dropbox > /dev/null && echo ; }
+
+while true; do
+	xsetroot -name "$(drb)  $(bat)  $(dat)"
+	sleep 60
+done
