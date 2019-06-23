d1923f345bea85f8fa45ebaa8fabbe42cf79510d
aleks <aleks.stier@icloud.com>
2019-06-23 18:31:32 +0200

    Cleant tstatus

--- scripts/tstatus.sh
+++ scripts/tstatus.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 battery=$(cat /sys/class/power_supply/BAT0/capacity)
-brightness=$(light | cut -d. -f1)
 date_time=$(date "+%R")
 mute=$(pulsemixer --get-mute)
 volume=$(pulsemixer --get-volume | cut -d" " -f1)
-echo "$date_time | BAT $battery | LIGHT $brightness | VOL $volume | MUTED $mute"
+echo "MUTED $mute | VOL $volume | BAT $battery | $date_time"
