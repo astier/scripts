3ec2d90578322dafbd9a790c48787d1e9a9fc08e
aleks <aleks.stier@icloud.com>
2019-08-01 00:30:58 +0200

    Tweak mint

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -4,28 +4,27 @@
 sudo add-apt-repository ppa:daniruiz/flat-remix
 sudo add-apt-repository ppa:neovim-ppa/stable
 apt update
-apt install arc-theme dropbox flat-remix git neovim nodejs xsel tmux deborphan build-essential libx11-dev libxft-dev yarn
-
-# REMOVE
-apt purge --auto-remove thunderbird hexchat-common libreoffice-common gnome-calendar gnome-calculator onboard-common rhythmbox-data xplayer-common transmission-common pix-data pix-data simple-scan vim-common ed nano flatpak gucharmap gnome-disk-utility xreader-common redshift xviewer gnome-font-viewer seahorse xed-common tomboy baobab gnome-logs gnome-power-manager gimp-data mono-runtime-common gnome-orca gnome-terminal-data timeshift dmz-cursor-theme
-apt purge --auto-remove "$(deborphan)"
-rm -fr Documents Music Pictures Public Templates Videos .themes .icons .bash_logout
+apt install arc-theme deborphan dropbox flat-remix git neovim nodejs tmux xsel yarn
 
 # PROJECTS
 mkdir ~/projects && cd ~/projects || exit
+git clone https://github.com/arcticicestudio/nord-gnome-terminal
 git clone https://github.com/astier/dotfiles
 git clone https://github.com/astier/scripts
-git clone https://github.com/astier/st
+bash nord-gnome-terminal/src/nord.sh
+rm nord-gnome-terminal
 cd dotfiles && sh setup.sh
 cd ../scripts && sh setup.sh
-cd ../st && sh setup.sh
+
+# REMOVE
+apt purge --auto-remove thunderbird hexchat-common libreoffice-common gnome-calendar gnome-calculator onboard-common rhythmbox-data xplayer-common transmission-common pix-data pix-data simple-scan vim-common ed nano flatpak gucharmap gnome-disk-utility xreader-common redshift xviewer gnome-font-viewer seahorse xed-common tomboy baobab gnome-logs gnome-power-manager gimp-data mono-runtime-common gnome-orca timeshift
+rm -fr Documents Music Pictures Public Templates Videos .themes .icons .bash_logout
+
+# UPDATE
+apt update && apt upgrade
+apt purge --auto-remove "$(deborphan)"
 
 # CONFIGURATI0N
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c PlugInstall
 sudo bash -c "echo 'vm.swappiness = 0' >> /etc/sysctl.conf"
-
-# UPDATE
-apt update
-apt upgrade
-apt purge --auto-remove "$(deborphan)"
