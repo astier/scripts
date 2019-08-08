fbf660fd90b096ed98fe8d6414e551bcef8e2af6
aleks <aleks.stier@icloud.com>
2019-08-08 12:01:51 +0200

    Use st instead of gnome-terminal in mint

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -4,25 +4,22 @@
 sudo add-apt-repository ppa:daniruiz/flat-remix
 sudo add-apt-repository ppa:neovim-ppa/stable
 apt update
-apt install arc-theme deborphan dropbox flat-remix git neovim nodejs tmux xsel yarn
+apt install arc-theme build-essential deborphan dropbox flat-remix git libx11-dev libxft-dev neovim nodejs tmux xsel yarn
 
 # PROJECTS
 mkdir ~/projects && cd ~/projects || exit
-git clone https://github.com/arcticicestudio/nord-gnome-terminal
 git clone https://github.com/astier/dotfiles
 git clone https://github.com/astier/scripts
-bash nord-gnome-terminal/src/nord.sh
-rm nord-gnome-terminal
+git clone https://github.com/astier/st
 cd dotfiles && sh setup.sh
 cd ../scripts && sh setup.sh
+cd ../st && sudo make install clean
 
-# REMOVE
+# REMOVE, UPDATE, CLEAN
 apt purge --auto-remove thunderbird hexchat-common libreoffice-common gnome-calendar gnome-calculator onboard-common rhythmbox-data xplayer-common transmission-common pix-data pix-data simple-scan vim-common ed nano flatpak gucharmap gnome-disk-utility xreader-common redshift xviewer gnome-font-viewer seahorse xed-common tomboy baobab gnome-logs gnome-power-manager gimp-data mono-runtime-common gnome-orca timeshift
-rm -fr Documents Music Pictures Public Templates Videos .themes .icons .bash_logout
-
-# UPDATE
 apt update && apt upgrade
 apt purge --auto-remove "$(deborphan)"
+rm -fr Documents Music Pictures Public Templates Videos .themes .icons .bash_logout
 
 # CONFIGURATI0N
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
