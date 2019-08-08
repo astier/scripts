d1a5ab2f25ca028844da15319e7da9c8a9958a16
aleks <aleks.stier@icloud.com>
2019-08-08 12:39:35 +0200

    Fix wrong path in mint

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -17,7 +17,7 @@ cd ../st && sudo make install clean
 apt purge --auto-remove thunderbird hexchat-common libreoffice-common gnome-calendar gnome-calculator onboard-common rhythmbox-data xplayer-common transmission-common pix-data pix-data simple-scan vim-common ed nano flatpak gucharmap gnome-disk-utility xreader-common redshift xviewer gnome-font-viewer seahorse xed-common tomboy baobab gnome-logs gnome-power-manager gimp-data mono-runtime-common gnome-orca timeshift
 apt update && apt upgrade
 apt purge --auto-remove "$(deborphan)"
-rm -fr Documents Music Pictures Public Templates Videos .themes .icons .bash_logout
+cd && rm -fr Documents Music Pictures Public Templates Videos .themes .icons .bash_logout
 
 # CONFIGURATI0N
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
