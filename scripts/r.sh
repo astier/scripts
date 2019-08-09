3175c75d5ff85d93f4483c14fccd7b0306fa14d5
aleks <aleks.stier@icloud.com>
2019-08-09 10:44:12 +0200

    Show also optional dependencies in r

--- scripts/r.sh
+++ scripts/r.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ $# -eq 0 ]; then
-	pacman -Qetq | fzf -m --preview 'pacman -Qi {1}' > /tmp/r
+	pacman -Qettq | fzf -m --preview 'pacman -Qi {1}' > /tmp/r
 	xargs -ra /tmp/r sudo pacman -Rns
 else
 	sudo pacman -Rns "$@"
