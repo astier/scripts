4b6307455ac615cbb900c8108e965c065fd97567
aleks <aleks.stier@icloud.com>
2019-08-18 23:02:54 +0200

    Tweak mint

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
+# Update sources and install kernel
 # apt install git
 # mkdir ~/projects && cd ~/projects
 # git clone https://github.com/astier/scripts
+# git checkout mint
 # sh scripts/scripts/mint.sh
 
 # INSTALL
@@ -15,14 +17,16 @@ apt install arc-theme build-essential deborphan dropbox ffmpeg flat-remix fonts-
 cd ~/projects || exit
 git clone https://github.com/astier/dotfiles
 git clone https://github.com/astier/st
-cd dotfiles && sh setup.sh
+cd dotfiles && git checkout mint && sh setup.sh
+cd ../scripts && sh setup.sh
 cd ../st && sudo make install clean
 
 # REMOVE, UPDATE, CLEAN
 apt purge --auto-remove thunderbird hexchat-common libreoffice-common gnome-calendar gnome-calculator onboard-common rhythmbox-data xplayer-common transmission-common pix-data pix-data simple-scan vim-common ed nano flatpak gucharmap gnome-disk-utility xreader-common redshift xviewer gnome-font-viewer seahorse xed-common tomboy baobab gnome-logs gnome-power-manager gnome-terminal gimp-data mono-runtime-common gnome-orca timeshift
-apt update && apt upgrade
 apt purge --auto-remove "$(deborphan)"
-cd && rm -fr Documents Music Pictures Public Templates Videos .themes .icons .bash_logout
+apt update && apt upgrade
+cd && rm -fr Documents Music Pictures Public Templates Videos .themes .icons .bash_logout .gtkrc-xfce .gtkrc-2.0
+cd ~/.config && rm -fr goa-1.0 hexchat caja
 
 # CONFIGURATI0N
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
