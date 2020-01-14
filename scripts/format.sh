18b7bce90c2adea8b8ba5058d7a6f722198bdc9c
aleks <aleks.stier@icloud.com>
2020-01-14 20:36:59 +0100

    [format] trim whitespace and trailing blank lines

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,6 +2,14 @@
 
 [ ! -f "$1" ] && echo File "$1" does not exist. && exit
 
+# TRIM (http://sed.sourceforge.net/sed1line.txt)
+# trailing whitespace
+sed -i 's/[ \t]*$//' "$1"
+# leading blank lines
+# sed -i '/./,$!d' "$1"
+# trailing blank lines
+sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' "$1"
+
 case $1 in
     *.py)
         isort "$1"
