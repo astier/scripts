d6ae9ee7264ae7780b9b06d4f2e5b6f126390305
aleks <aleks.stier@icloud.com>
2020-01-28 05:10:38 +0100

    [autopen|execute] drop support for config.def.h

--- scripts/autopen.sh
+++ scripts/autopen.sh
@@ -6,8 +6,6 @@ elif [ -r main.tex ]; then
     $EDITOR main.tex
 elif [ -r setup.sh ]; then
     $EDITOR setup.sh
-elif [ -r config.def.h ]; then
-    $EDITOR config.def.h
 else
     echo No appropriate file found.
 fi
