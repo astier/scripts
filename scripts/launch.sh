676a98f3fdc408c494bb5699b2c5da34195d1a8c
aleks <aleks.stier@icloud.com>
2020-01-28 14:49:54 +0100

    [launch] refactor command-gathering from fzf-terminal

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-$TERMINAL --class launcher -d 32 8 -e sh -c "nohup \$(printf '%s' $PATH | xargs -d: -I{} find -L {} -maxdepth 1 -mindepth 1 -executable -type f -printf '%P\\n' | sort -u | fzf) >/dev/null 2>&1"
+cmds=$(printf "%s" "$PATH" | xargs -d: -I{} find -L {} -maxdepth 1 -mindepth 1 -executable -type f -printf "%P\n" | sort -u)
+$TERMINAL --class launcher -d 32 8 -e sh -c "nohup \$(echo \"$cmds\" | fzf) >/dev/null 2>&1"
