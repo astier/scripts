4db7c69d599d5754a2b272e8f2899fd41dcb0431
aleks <aleks.stier@icloud.com>
2020-01-28 15:42:33 +0100

    [setup] include script-extension

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
 
 install() {
-    sudo ln -frs scripts/"$1".sh /usr/local/bin/"$1" &&
+    install_name=$(echo "$1" | cut -d. -f1)
+    sudo ln -frs scripts/"$1" /usr/local/bin/"$install_name" &&
         echo Installed: "$1"
 }
 
-install autopen
-install bstatus
-install cpm
-install efistub
-install execute
-install format
-install launch
-install lint
-install open
-install pkg
-install wal
+install autopen.sh
+install bstatus.sh
+install cpm.sh
+install efistub.sh
+install execute.sh
+install format.sh
+install launch.sh
+install lint.sh
+install open.sh
+install pkg.sh
+install wal.sh
