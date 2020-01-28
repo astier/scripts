70ca616dd41cfad67715392782ebcc985970e1a1
aleks <aleks.stier@icloud.com>
2020-01-28 05:38:15 +0100

    [launch|bstatus] adjust terminal to alacritty

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-$TERMINAL -n launcher sh -c "nohup \$(printf \"%s\" \"\$PATH\" | xargs -d : -I {} find -L {} -maxdepth 1 -executable -type f -printf \"%P\\n\" | sort -u | fzf) >/dev/null 2>&1" >/dev/null 2>&1
+$TERMINAL --class launcher -d 32 8 -e sh -c "nohup \$(printf \"%s\" \"\$PATH\" | xargs -d : -I {} find -L {} -maxdepth 1 -executable -type f -printf \"%P\\n\" | sort -u | fzf) >/dev/null 2>&1" >/dev/null 2>&1
