82a372df318fae9fee5f61e87a3396fc66ccde4f
aleks <aleks.stier@icloud.com>
2020-03-19 03:27:40 +0100

    [launch] double-quote to prevent warning

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 cmd=$(printf "%s" "$PATH" | xargs -d: -I{} find -L {} -maxdepth 1 -mindepth 1 -executable -type f -printf "%P\n" | sort -u | fzf)
-nohup spawn $cmd >/dev/null 2>&1
+nohup spawn "$cmd" >/dev/null 2>&1
