022e754a22790548c7e1a2024ebd5e1144d4de62
aleks <aleks.stier@icloud.com>
2020-07-06 00:04:25 +0200

    [sfzf] simplify

--- scripts/sfzf.sh
+++ scripts/sfzf.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env sh
 
-[ $# -eq 0 ] && return
-
-[ -t 0 ] && matches=$(find -type f $FIND_ARGS | cut -c3-) || matches=$(cat)
-[ -z "$matches" ] && matches=$(find -type f $FIND_ARGS | cut -c3-)
-
-for pattern in "$@"; do
-    patterns="$patterns\|$pattern"
-    matches=$(echo "$matches" | grep "$pattern")
-done
-matches=$(echo "$matches" | awk '{print length, $0}' | sort -nrs | cut -d" " -f2)
-
-if [ -n "$matches" ]; then
-    echo "$matches" | grep --color=always "$patterns" >&2
-    echo "$matches" | tail -n1
-fi
+matches=$(cat)
+for pattern in "$@"; do matches=$(echo "$matches" | grep "$pattern"); done
+echo "$matches" | awk 'NR==1 || length<len {len=length; line=$0} END {print line}'
