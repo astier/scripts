efa8073aa0f10cfa860adc43508ee678d22fec8c
aleks <aleks.stier@icloud.com>
2020-07-06 19:39:35 +0200

    [ffind] quote patterns

--- scripts/ffind.sh
+++ scripts/ffind.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-find . "$@" ! -path */.git/* ! -name .git ! -path */.idea/* ! -path */__pycache__/* ! -iname *.png ! -name *.toc ! -name *.fdb_latexmk ! -name *.out ! -name *.log ! -name *.fls ! -name *.gz ! -name *.tox ! -name *.nav ! -name *.snm ! -name *.aux ! -name tags ! -name . | cut -c3-
+find . "$@" ! -path '*/.git/*' ! -name .git ! -path '*/.idea/*' ! -path '*/__pycache__/*' ! -iname '*.png' ! -name '*.toc' ! -name '*.fdb_latexmk' ! -name '*.out' ! -name '*.log' ! -name '*.fls' ! -name '*.gz' ! -name '*.tox' ! -name '*.nav' ! -name '*.snm' ! -name '*.aux' ! -name tags ! -name . | cut -c3-
