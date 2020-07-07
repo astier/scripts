10a8180b460dc6cc0d470bdc00ecc1d948c92e24
aleks <aleks.stier@icloud.com>
2020-07-07 11:05:41 +0200

    [sfzf] ignore case

--- scripts/sfzf.sh
+++ scripts/sfzf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 matches=$(cat)
-for pattern in "$@"; do matches=$(echo "$matches" | grep "$pattern"); done
+for pattern in "$@"; do matches=$(echo "$matches" | grep -i "$pattern"); done
 echo "$matches" | awk 'NR==1 || length<rank {rank=length; line=$0} END {print line}'
