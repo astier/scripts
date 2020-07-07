0da1fdbd578856c2122c940d9c3e7584823fe605
aleks <aleks.stier@icloud.com>
2020-07-07 10:59:18 +0200

    [sfzf] rename len to rank

--- scripts/sfzf.sh
+++ scripts/sfzf.sh
@@ -2,4 +2,4 @@
 
 matches=$(cat)
 for pattern in "$@"; do matches=$(echo "$matches" | grep "$pattern"); done
-echo "$matches" | awk 'NR==1 || length<len {len=length; line=$0} END {print line}'
+echo "$matches" | awk 'NR==1 || length<rank {rank=length; line=$0} END {print line}'
