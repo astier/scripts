a62d7b02c734b036f169bdddaed4772e970a6ab8
aleks <aleks.stier@icloud.com>
2020-07-09 15:38:36 +0200

    [launch] sleep for .01

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,4 +2,4 @@
 
 cmd=$(printf "%s" "$PATH" | xargs -d: -I{} find -L {} -maxdepth 1 -mindepth 1 -executable -type f -printf "%P\n" | sort -u | fzf)
 nohup "$cmd" >/dev/null 2>&1 &
-sleep .001
+sleep .01
