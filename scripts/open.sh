601921679ffdf2e96c0e9b18b189695ff9e13df5
aleks <aleks.stier@icloud.com>
2020-10-09 18:25:27 +0200

    [open] dont implement fzf

--- scripts/open.sh
+++ scripts/open.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env sh
 
-if [ $# -eq 0 ]; then
-    fzf | xargs -r open
-elif [ ! -f "$1" ]; then
-    fzf --filter="$*" | head -n1 | xargs -r open
-else
-    mimetype=$(file -bL --mime-type "$1")
-    mime=$(echo "$mimetype" | cut -d/ -f1)
-    case $mime in
-        text) $EDITOR "$@" ;;
-        video) mpv "$@" ;;
-        audio) mpv "$@" ;;
-        image) eog "$@" ;;
-        *)
-            case $mimetype in
-                application/pdf) $BROWSER "$@" & ;;
-                application/csv) $EDITOR "$@" ;;
-                application/json) $EDITOR "$@" ;;
-                application/octet-stream) $EDITOR "$@" ;;
-                *document*) libreoffice "$@" 2>/dev/null & ;;
-                inode/x-empty) $EDITOR "$@" ;;
-                *) echo No association with mimetype: "$mimetype" >&2 ;;
-            esac
-            ;;
-    esac
-fi
+mimetype=$(file -bL --mime-type "$1")
+case "$(echo "$mimetype" | cut -d/ -f1)" in
+    text) $EDITOR "$@" ;;
+    video) mpv "$@" ;;
+    audio) mpv "$@" ;;
+    image) eog "$@" ;;
+    *)
+        case $mimetype in
+            application/pdf) $BROWSER "$@" & ;;
+            application/csv) $EDITOR "$@" ;;
+            application/json) $EDITOR "$@" ;;
+            application/octet-stream) $EDITOR "$@" ;;
+            *document*) libreoffice "$@" 2>/dev/null & ;;
+            inode/x-empty) $EDITOR "$@" ;;
+            *) echo No association with mimetype: "$mimetype" >&2 ;;
+        esac
+        ;;
+esac
