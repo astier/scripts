1ae5ee693c3be0c0fe97d12c28747312063e5c3e
aleks <aleks.stier@icloud.com>
2020-11-06 11:50:48 +0100

    [efistub] use offset:size for cryptkey

--- efistub.sh
+++ efistub.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 sudo efibootmgr -Bqb0
-sudo efibootmgr -cqd /dev/nvme0n1 -p1 -L arch -l /vmlinuz-linux-lts -u 'initrd=\intel-ucode.img initrd=\initramfs-linux-lts.img cryptdevice=/dev/nvme0n1p2:root cryptkey=/dev/sda:vfat:key root=LABEL=ROOT rw loglevel=2 udev.log_priority=3 nowatchdog module_blacklist=iTCO_vendor_support,iTCO_wdt vt.global_cursor_default=0'
+sudo efibootmgr -cqd /dev/nvme0n1 -p1 -L arch -l /vmlinuz-linux-lts -u 'initrd=\intel-ucode.img initrd=\initramfs-linux-lts.img cryptdevice=/dev/nvme0n1p2:root cryptkey=/dev/sda:offset:size root=LABEL=ROOT rw loglevel=2 udev.log_priority=3 nowatchdog module_blacklist=iTCO_vendor_support,iTCO_wdt vt.global_cursor_default=0'
 efibootmgr
