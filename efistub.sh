2dd301555b90e79cfae036addae1ce4c81b1f5de
aleks <aleks.stier@icloud.com>
2021-02-01 20:07:09 +0100

    [efistub] configure linux instead of linux-lts

--- efistub.sh
+++ efistub.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 doas efibootmgr -Bqb0
-doas efibootmgr -cqd /dev/nvme0n1 -p1 -L arch -l /vmlinuz-linux-lts -u 'initrd=\intel-ucode.img initrd=\initramfs-linux-lts.img cryptdevice=/dev/nvme0n1p2:root cryptkey=/dev/sda:17408:512 root=LABEL=ROOT rw loglevel=2 udev.log_priority=3 nowatchdog module_blacklist=iTCO_vendor_support,iTCO_wdt vt.global_cursor_default=0'
+doas efibootmgr -cqd /dev/nvme0n1 -p1 -L arch -l /vmlinuz-linux -u 'initrd=\intel-ucode.img initrd=\initramfs-linux.img cryptdevice=/dev/nvme0n1p2:root cryptkey=/dev/sda:17408:512 root=LABEL=ROOT rw loglevel=2 udev.log_priority=3 nowatchdog module_blacklist=iTCO_vendor_support,iTCO_wdt vt.global_cursor_default=0'
 efibootmgr
