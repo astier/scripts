3a9ef10b25441a954ffdd02cf9c8765788274391
aleks <aleks.stier@icloud.com>
2021-03-19 15:10:55 +0100

    [open] open files with sudo if not writable

--- open.sh
+++ open.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env sh
 
+edit() {
+    if [ -e "$1" ] && [ ! -w "$1" ]; then
+        sudo "$EDITOR" "$@"
+    elif [ ! -e "$1" ] && [ -z "$(find . -maxdepth 0 -user "$USER")" ]; then
+        sudo "$EDITOR" "$@"
+    else
+        "$EDITOR" "$@"
+    fi
+}
+
 if [ $# = 0 ]; then
     fzf | xargs -r "$OPENER" && return
 elif [ ! -f "$1" ]; then
-    "$EDITOR" "$1" && return
+    edit "$@" && return
 fi
 
 mimetype=$(file -bL --mime-type "$1")
 case "$(echo "$mimetype" | cut -d/ -f1)" in
-    text) $EDITOR "$@" ;;
+    text) edit "$@" ;;
     video) $VIEWER_VID "$@" & ;;
     audio) $VIEWER_MP3 "$@" & ;;
     image) $VIEWER_IMG "$@" 2>/dev/null & ;;
     *)
         case $mimetype in
             application/pdf) $BROWSER "$@" & ;;
-            application/csv) $EDITOR "$@" ;;
-            application/json) $EDITOR "$@" ;;
-            application/octet-stream) $EDITOR "$@" ;;
+            application/csv) edit "$@" ;;
+            application/json) edit "$@" ;;
+            application/octet-stream) edit "$@" ;;
             *document*) libreoffice "$@" 2>/dev/null & ;;
-            inode/x-empty) $EDITOR "$@" ;;
+            inode/x-empty) edit "$@" ;;
             *) echo No association with mimetype: "$mimetype" >&2 ;;
         esac
         ;;
