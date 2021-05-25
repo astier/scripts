f8cf5ced766988effb007ab8e5ba417e366fe14e
aleks <aleks.stier@icloud.com>
2021-05-25 23:20:05 +0200

    [autopen] quote $EDITOR

--- autopen.sh
+++ autopen.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
 if [ -r main.py ]; then
-    $EDITOR main.py
+    "$EDITOR" main.py
 elif [ -r main.tex ]; then
-    $EDITOR main.tex
+    "$EDITOR" main.tex
 elif [ -r setup.sh ]; then
-    $EDITOR setup.sh
+    "$EDITOR" setup.sh
 elif [ -r config.def.h ]; then
-    $EDITOR config.def.h
+    "$EDITOR" config.def.h
 elif [ -r config.h ]; then
-    $EDITOR config.h
+    "$EDITOR" config.h
 elif [ -r Makefile ]; then
-    $EDITOR Makefile
+    "$EDITOR" Makefile
 else
     echo No appropriate file found.
 fi
