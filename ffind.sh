c83a9b2b880d39f41175839b3df6192252e30434
aleks <aleks.stier@icloud.com>
2021-08-17 13:31:21 +0200

    [ffind] quote parameters

--- ffind.sh
+++ ffind.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env sh
 
 find -L . "$@" \
-    ! -name *.aux \
-    ! -name *.fdb_latexmk \
-    ! -name *.fls \
-    ! -name *.gz \
-    ! -name *.log \
-    ! -name *.nav \
-    ! -name *.out \
-    ! -name *.png \
-    ! -name *.snm \
-    ! -name *.toc \
-    ! -name *.tox \
+    ! -name "*.aux" \
+    ! -name "*.fdb_latexmk" \
+    ! -name "*.fls" \
+    ! -name "*.gz" \
+    ! -name "*.log" \
+    ! -name "*.nav" \
+    ! -name "*.out" \
+    ! -name "*.png" \
+    ! -name "*.snm" \
+    ! -name "*.toc" \
+    ! -name "*.tox" \
     ! -name . \
     ! -name .ccls-cache \
     ! -name .git \
     ! -name .idea \
     ! -name .vscode \
     ! -name tags \
-    ! -path */.ccls-cache/* \
-    ! -path */.git/* \
-    ! -path */.idea/* \
-    ! -path */.vscode/* \
-    ! -path */__pycache__/* \
+    ! -path "*/.ccls-cache/*" \
+    ! -path "*/.git/*" \
+    ! -path "*/.idea/*" \
+    ! -path "*/.vscode/*" \
+    ! -path "*/__pycache__/*" \
 | cut -c3-
