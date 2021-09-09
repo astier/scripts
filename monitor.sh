fee7cc7b2aa8348befec176ea606e90f24fc662e
aleks <aleks.stier@icloud.com>
2021-09-09 11:53:27 +0200

    [monitor] echo some messages

--- monitor.sh
+++ monitor.sh
@@ -3,13 +3,15 @@
 # Exit if Xorg is not running
 ! pidof Xorg > /dev/null && exit
 # Exit if autorandr is installed
-find /usr/bin/ -type f -name autorandr -print -quit | grep -q . && echo exit: autorandr is installed && exit
+find /usr/bin/ -type f -name autorandr -print -quit | grep -q . && echo Exit: autorandr is installed && exit
 
 intern=eDP-1
 extern=HDMI-1
 
 if xrandr | grep -q "$extern connected"; then
     xrandr --output "$intern" --off --output "$extern" --auto
+    echo Switched to extern monitor "$extern"
 else
     xrandr --output "$extern" --off --output "$intern" --auto
+    echo Switched to intern monitor "$intern"
 fi
