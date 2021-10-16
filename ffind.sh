70f79ff0834925c0429eccd6643e4d9f9d07943f
aleks <aleks.stier@icloud.com>
2021-10-17 01:16:39 +0200

    [ffind] dont ignore latex-related files and png

--- ffind.sh
+++ ffind.sh
@@ -3,22 +3,12 @@
 export LC_ALL="C"
 
 find -L . \
-    ! -name "*.aux" \
-    ! -name "*.fdb_latexmk" \
-    ! -name "*.fls" \
-    ! -name "*.gz" \
-    ! -name "*.log" \
-    ! -name "*.nav" \
-    ! -name "*.out" \
-    ! -name "*.png" \
-    ! -name "*.snm" \
-    ! -name "*.toc" \
-    ! -name "*.tox" \
     ! -name . \
     ! -name .ccls-cache \
     ! -name .git \
     ! -name .idea \
     ! -name .vscode \
+    ! -name __pycache__ \
     ! -name tags \
     ! -path "*/.ccls-cache/*" \
     ! -path "*/.git/*" \
