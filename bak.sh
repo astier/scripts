4f752282320e358d19bc77cdd30f9db9173088ab
aleks <aleks.stier@icloud.com>
2021-10-22 01:17:53 +0200

    [bak] verify checksum

--- bak.sh
+++ bak.sh
@@ -5,7 +5,7 @@
 sudo mkdir -p "$DEST"
 sudo cryptsetup open "$DEVICE" "$DEVICE_NAME" -d "$KEY"
 sudo mount "$PARTITION" "$DEST" \
-    && sudo rsync -ahiu --delete --info=name --inplace --exclude .~* "$SRC" "$DEST"
+    && sudo rsync -achiu --delete --info=name --inplace --exclude .~* "$SRC" "$DEST"
 sudo umount "$DEST"
 sudo cryptsetup close "$DEVICE_NAME"
 sudo rmdir "$DEST"
