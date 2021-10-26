b5ca4a5813222d3730aedaa423bed7dc4463482c
aleks <aleks.stier@icloud.com>
2021-10-27 01:02:17 +0200

    [menu][fix] sh: 0: getcwd() failed: No such file or directory

--- menu.sh
+++ menu.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
+# 'cd' fixes: sh: 0: getcwd() failed: No such file or directory
+# Reproduce with:
+# st -n popup -ig 32x8+400+400 sh -c "nvim >/dev/null 2>&1"
+cd /tmp
 exec st -n popup -ig 32x8+400+400 sh -c "fzf $* < /proc/$$/fd/0 > /proc/$$/fd/1"
