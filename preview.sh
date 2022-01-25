cc2c010c3476429fcd1c7eff9348e851b19e77dd
aleks <aleks.stier@icloud.com>
2022-01-25 20:49:02 +0100

    [preview] delete unnecessary returns

--- preview.sh
+++ preview.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 if [ $# = 0 ] || [ -z "$1" ]; then
-    ls -Av --color --group-directories-first && return
+    ls -Av --color --group-directories-first
 elif [ -d "$1" ]; then
-    ls -Av --color --group-directories-first "$1" && return
+    ls -Av --color --group-directories-first "$1"
 elif [ ! -f "$1" ]; then
-    echo File "$1" does not exist. && return
+    echo File "$1" does not exist.
+else
+    case "$1" in
+        *) head -n "${2:-29}" "$1" ;;
+    esac
 fi
-
-case "$1" in
-    *) head -n "${2:-29}" "$1" ;;
-esac
