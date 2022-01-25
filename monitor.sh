ab295068678e8e826b4319ca3ab0dfd2ee457007
aleks <aleks.stier@icloud.com>
2022-01-25 22:38:59 +0100

    [monitor] detect LVDS-/eDP-1 and VGA-/HDMI-1 monitors

--- monitor.sh
+++ monitor.sh
@@ -2,16 +2,25 @@
 
 # Exit if Xorg is not running
 ! pidof Xorg > /dev/null && exit
-# Exit if autorandr is installed
-find /usr/bin/ -type f -name autorandr -print -quit | grep -q . && echo Exit: autorandr is installed && exit
 
-intern=eDP-1
-extern=HDMI-1
+configure() { xrandr --output "$1" --auto --primary --output "$2" --off; }
+
+INFO=/tmp/xrandr_dump
+xrandr > "$INFO"
+
+if grep -q "LVDS-1 connected" "$INFO"; then
+    intern=LVDS-1
+    extern=VGA-1
+elif grep -q "eDP-1 connected" "$INFO"; then
+    intern=eDP-1
+    extern=HDMI-1
+else
+    echo Internal monitor could not be detected.
+    exit
+fi
 
 if xrandr | grep -q "$extern connected"; then
-    xrandr --output "$intern" --off --output "$extern" --auto
-    echo Switched to extern monitor "$extern"
+    configure "$extern" "$intern"
 else
-    xrandr --output "$extern" --off --output "$intern" --auto
-    echo Switched to intern monitor "$intern"
+    configure "$intern" "$extern"
 fi
