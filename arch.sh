4d8c15fa7ddafbb54bf95f602636f4c51c2192df
aleks <aleks.stier@icloud.com>
2022-03-04 02:13:57 +0100

    Switch from TERMINAL to tty

--- arch.sh
+++ arch.sh
@@ -19,7 +19,6 @@ mount -L BOOT /mnt/boot
 pacstrap /mnt \
     alsa-utils \
     arc-gtk-theme \
-    autorandr \
     base \
     bash-completion \
     fakeroot \
@@ -39,8 +38,6 @@ pacstrap /mnt \
     noto-fonts-emoji \
     opendoas \
     openssh \
-    patch \
-    pkgconf \
     sx \
     terminus-font \
     tmux \
@@ -104,19 +101,16 @@ mkdir repos && cd repos
 git clone git@github.com:astier/config.git
 git clone git@github.com:astier/scripts.git
 git clone git@github.com:astier/sswm.git
-git clone git@github.com:astier/st.git
 git clone https://aur.archlinux.org/paru-bin
 
 # REPOS - INSTALL
 cd config && . .profile && ./setup.sh
 cd ../scripts && ./setup.sh
 cd ../sswm && make install
-cd ../st && make install
 cd ../paru-bin && makepkg -is
 paru -S \
     dashbinsh \
     flat-remix \
-    libxft-bgra \
     lux \
     nerd-fonts-hack \
 
