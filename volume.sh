55eefde54dafdb97d64fd26098200760bae6c4ad
aleks <aleks.stier@icloud.com>
2022-04-13 11:17:53 +0200

    [volume] ? shows volume only for one earphone

--- volume.sh
+++ volume.sh
@@ -4,7 +4,7 @@ case $1 in
     -) pulsemixer --change-volume -5 ;;
     +) pulsemixer --change-volume +5 ;;
     =) pulsemixer --toggle-mute ;;
-    ?) pulsemixer --get-volume ;;
+    ?) pulsemixer --get-volume | cut -d' ' -f1 ;;
     *) pulsemixer
 esac
 
