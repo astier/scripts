1b011ee07d15e31eab43a2f7d656314aa3460d09
aleks <aleks.stier@icloud.com>
2022-04-21 15:19:04 +0200

    [bak] implement -r option for reverse sync

--- bak.sh
+++ bak.sh
@@ -2,10 +2,17 @@
 
 . "$XDG_CONFIG_HOME/bakrc"
 
+backup() { sudo rsync -ahiu --delete --info=name --inplace --exclude .~* "$1" "$2" ; }
+
 sudo mkdir -p "$DEST"
 sudo cryptsetup open "$DEVICE" "$DEVICE_NAME" -d "$KEY"
-sudo mount "$PARTITION" "$DEST" \
-    && sudo rsync -achiu --delete --info=name --inplace --exclude .~* "$SRC" "$DEST"
+sudo mount "$PARTITION" "$DEST"
+
+case $1 in
+    -r) backup "$DEST" "$SRC" ;;
+    *)  backup "$SRC" "$DEST" ;;
+esac
+
 sudo umount "$DEST"
 sudo cryptsetup close "$DEVICE_NAME"
 sudo rmdir "$DEST"
