dcd14238adad38680c521f578cf53b0f5cae35b4
aleks <aleks.stier@icloud.com>
2022-05-30 00:39:25 +0200

    [volume] use steps of 5 to change volume

--- volume.sh
+++ volume.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 case $1 in
-    -) pulsemixer --change-volume -10 ;;
-    +) pulsemixer --change-volume +10 ;;
+    -) pulsemixer --change-volume -5 ;;
+    +) pulsemixer --change-volume +5 ;;
     =) pulsemixer --toggle-mute ;;
     ?) pulsemixer --get-volume | cut -d' ' -f1 ;;
     *) pulsemixer
