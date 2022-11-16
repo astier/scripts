4fab40d1931a9c12694c79d246fa8337da8b9c3a
Aleksandrs Stier <aleks.stier@icloud.com>
2022-11-17 00:47:26 +0100

    [bstatus] use TERMINAL to notify if dunst doesn't run

--- bstatus.sh
+++ bstatus.sh
@@ -4,7 +4,13 @@ BATTERY=/sys/class/power_supply/BAT0
 
 [ ! -d "$BATTERY" ] && echo NO BATTERY FOUND && exit 1
 
-notify() { dunstify -h string:x-dunst-stack-tag:battery -u critical "Battery: $1" ; }
+notify() {
+    if pidof -q dunst; then
+        dunstify -h string:x-dunst-stack-tag:battery -u critical "Battery: $1"
+    else
+        nohup setsid -f "$TERMINAL" -e sh -c "echo BATTERY $1; read _" > /dev/null 2>&1
+    fi
+}
 
 loop() {
     if [ "$(pgrep -f "bstatus -l" | wc -l)" -gt 2 ]; then
