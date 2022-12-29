6e10969c52c6ba3c8dc44ce783bd438d5232c291
Aleksandrs Stier <aleks.stier@icloud.com>
2022-12-29 20:12:12 +0500

    [brightness] quote ? in case-statement

--- brightness.sh
+++ brightness.sh
@@ -5,7 +5,7 @@ STEP_SIZE=10%
 case $1 in
     -) lux -s "$STEP_SIZE" > /dev/null ;;
     +) lux -a "$STEP_SIZE" > /dev/null ;;
-    ?) lux -G ;;
+    "?") lux -G ;;
     *) lux -S "$1%" > /dev/null ;;
 esac
 
