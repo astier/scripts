e8d959655d0da3a6040f731e4d78c07cde61f1f7
Aleksandrs Stier <aleks.stier@icloud.com>
2023-01-05 12:47:54 +0500

    [launch] exit if no program selected by fzf

--- launch.sh
+++ launch.sh
@@ -15,8 +15,9 @@ case $1 in
         chvt 1
         ;;
     fzf|*)
-        spawn "$(printf "%s" "$PATH" | xargs -d: -I{} find -L {} -maxdepth 1 \
-            -mindepth 1 -executable -type f -printf "%P\n" | sort -u | fzf)"
+        cmd=$(printf "%s" "$PATH" | xargs -d: -I{} find -L {} -maxdepth 1 \
+            -mindepth 1 -executable -type f -printf "%P\n" | sort -u | fzf)
+        [ -n "$cmd" ] && spawn "$cmd" || exit
         ;;
 esac
 
