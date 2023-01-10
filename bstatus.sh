d6851325233a120f574fa869cd5d0d5d26690778
Aleksandrs Stier <aleks.stier@icloud.com>
2023-01-10 11:35:12 +0500

    [bstatus] dont blink, just suspend

--- bstatus.sh
+++ bstatus.sh
@@ -4,25 +4,15 @@ BATTERY=/sys/class/power_supply/BAT0
 
 [ ! -d "$BATTERY" ] && echo NO BATTERY FOUND && exit 1
 
-blink() {
-    old_brightness=$(brightness ?)
-    brightness 1% && sleep 1
-    brightness "$old_brightness" && sleep 1
-}
-
 loop() {
     if [ "$(pgrep -f "bstatus -l" | wc -l)" -gt 2 ]; then
         echo An instance is already running. && exit 1
     fi
     while true; do
-        capacity=$(cat $BATTERY/capacity)
         status=$(cat $BATTERY/status)
         if [ "$status" = "Discharging" ]; then
-            if [ "$capacity" -le 10 ]; then
-                blink && blink
-            elif [ "$capacity" -le 5 ]; then
-                systemctl suspend
-            fi
+            capacity=$(cat $BATTERY/capacity)
+            [ "$capacity" -le 5 ] && systemctl suspend
         fi
         sleep 1m
     done
