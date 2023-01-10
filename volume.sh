87cb183aed8e368f9a10f710dec26f29a76f2de9
Aleksandrs Stier <aleks.stier@icloud.com>
2023-01-10 15:52:41 +0500

    [volume] replace amixer with wpctl

--- volume.sh
+++ volume.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 case $1 in
-    -) amixer set Master 1%- > /dev/null ;;
-    +) amixer set Master 1%+ > /dev/null ;;
-    =) amixer set Master toggle > /dev/null ;;
-    ?) amixer get Master | tail -n1 | awk -F'[][]' '{ print $2 }' ;;
-    *) alsamixer
+    -) wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%- ;;
+    +) wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+ ;;
+    =) wpctl set-mute   @DEFAULT_AUDIO_SINK@ toggle ;;
+    ?) wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d. -f2 ;;
+    *) wpctl status
 esac
 
