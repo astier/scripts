d272343910b0991c75fb041b53e9d9b683a14a8a
Aleksandrs Stier <aleks.stier@icloud.com>
2023-01-10 11:37:25 +0500

    [bstatus] execute sleep as while-condition

--- bstatus.sh
+++ bstatus.sh
@@ -8,13 +8,12 @@ loop() {
     if [ "$(pgrep -f "bstatus -l" | wc -l)" -gt 2 ]; then
         echo An instance is already running. && exit 1
     fi
-    while true; do
+    while sleep 2m; do
         status=$(cat $BATTERY/status)
         if [ "$status" = "Discharging" ]; then
             capacity=$(cat $BATTERY/capacity)
             [ "$capacity" -le 5 ] && systemctl suspend
         fi
-        sleep 1m
     done
 }
 
