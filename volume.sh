8cfe1616cac40f278826e51a3d7b9da53ac27b14
Aleksandrs Stier <aleks.stier@icloud.com>
2023-01-12 02:31:40 +0500

    [volume] dont print % for volume

--- volume.sh
+++ volume.sh
@@ -4,7 +4,7 @@ case $1 in
     -) amixer set Master 1%- > /dev/null ;;
     +) amixer set Master 1%+ > /dev/null ;;
     =) amixer set Master toggle > /dev/null ;;
-    ?) amixer get Master | tail -n1 | awk -F'[][]' '{ print $2 }' ;;
+    ?) amixer get Master | tail -n1 | awk -F'[][]' '{ print $2 }' | sed s/%// ;;
     *) alsamixer
 esac
 
