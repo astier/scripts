d06d710bc3afe09a18996ce5cd4c31bc9bf0a08f
Aleksandrs Stier <aleks.stier@icloud.com>
2023-01-15 01:48:46 +0500

    [volume] change volume by 4%

--- volume.sh
+++ volume.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 case $1 in
-    -) pulsemixer --change-volume -2 ;;
-    +) pulsemixer --change-volume +2 ;;
+    -) pulsemixer --change-volume -4 ;;
+    +) pulsemixer --change-volume +4 ;;
     =) pulsemixer --toggle-mute ;;
     ?) pulsemixer --get-volume | cut -d' ' -f1 ;;
     *) pulsemixer
