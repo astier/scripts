49918ef5f805dea6275d7247c9bcf7cf07c05f79
Aleksandrs Stier <aleks.stier@icloud.com>
2023-02-15 18:26:42 +0500

    [iwltm] remove unnecessary quotes

--- iwltm.sh
+++ iwltm.sh
@@ -3,25 +3,25 @@
 # iwltm - I Would Like T-o/alk/o/he/mux-Manager.
 
 DIR=/tmp/iwltm
-[ ! -d "$DIR" ] && mkdir -p "$DIR"
+[ ! -d $DIR ] && mkdir -p $DIR
 
-FILE="$DIR/pane_id"
-[ ! -f "$FILE" ] && echo " " > "$FILE"
+FILE=$DIR/pane_id
+[ ! -f $FILE ] && echo " " > $FILE
 
-pane_id="$(cat "$FILE")"
+pane_id=$(cat $FILE)
 
 send() {
     # Check if pane exists. If not create new pane.
     if ! tmux has-session -t "$pane_id" > /dev/null 2>&1; then
-        pane_id="$(tmux new-window -ac "#{pane_current_path}" -PF "#{pane_id}")"
-        echo "$pane_id" > "$FILE"
+        pane_id=$(tmux new-window -ac "#{pane_current_path}" -PF "#{pane_id}")
+        echo "$pane_id" > $FILE
         # Give shell time to load so send-keys doesnt print keys twice
         sleep 0.1
     fi
     tmux send-keys -t "$pane_id" "$*" ENTER
     # Switch to pane-window if pane-window is not focused
-    WINDOW_ID_CURRENT="$(tmux display-message -p "#{window_id}")"
-    WINDOW_ID_PANE_ID="$(tmux display-message -pt "$pane_id" "#{window_id}")"
+    WINDOW_ID_CURRENT=$(tmux display-message -p "#{window_id}")
+    WINDOW_ID_PANE_ID=$(tmux display-message -pt "$pane_id" "#{window_id}")
     if [ "$WINDOW_ID_CURRENT" != "$WINDOW_ID_PANE_ID" ]; then
         tmux select-window -t "$pane_id"
     fi
@@ -29,11 +29,11 @@ send() {
 
 [ $# = 0 ] && echo "[--get] [--mark] [--send keys]" && exit
 
-flag="$1"
+flag=$1
 shift
 
-case "$flag" in
+case $flag in
     --get)  echo "$pane_id" ;;
-    --mark) [ -n "$TMUX_PANE" ] && echo "$TMUX_PANE" > "$FILE" ;;
+    --mark) [ -n "$TMUX_PANE" ] && echo "$TMUX_PANE" > $FILE ;;
     --send) send "$@" ;;
 esac
