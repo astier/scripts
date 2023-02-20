21c5447ebb6ddbf36c6b2d2f9d4dd97539a3d830
Aleksandrs Stier <aleks.stier@icloud.com>
2023-02-21 03:35:23 +0500

    [ggrep] exclude .ccls-cache, .git, .idea, __pycache__

--- ggrep.sh
+++ ggrep.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-exec grep -IRn --color=auto --exclude-dir=.?* "$@"
+exec grep -IRn --color=auto \
+    --exclude-dir=.ccls-cache \
+    --exclude-dir=.git \
+    --exclude-dir=.idea \
+    --exclude-dir=__pycache__ \
+"$@"
