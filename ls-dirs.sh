0d3b8e32c261f8f3e36a2f47e912b751f38925bb
Aleksandrs Stier <aleks.stier@icloud.com>
2023-02-28 19:38:41 +0500

    [ls-dirs/-files] check if git is instaled

--- ls-dirs.sh
+++ ls-dirs.sh
@@ -2,7 +2,7 @@
 
 if [ -x "$(command -v rg)" ]; then
     exec rg --files --no-messages -0 | xargs -0 dirname | sed /^\.$/d | sort -u
-elif git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+elif [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     exec git ls-files -z | xargs -0 dirname | sed /^\.$/d | sort -u
 else
     exec ffind -type d 2> /dev/null
--- ls-files.sh
+++ ls-files.sh
@@ -2,7 +2,7 @@
 
 if [ -x "$(command -v rg)" ]; then
     exec rg --files --no-messages
-elif git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+elif [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     exec git ls-files
 else
     exec ffind -type f 2> /dev/null
