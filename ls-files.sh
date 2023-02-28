cf40d53c70518f114ba5685e9b1c67c3d212583f
Aleksandrs Stier <aleks.stier@icloud.com>
2023-02-28 22:42:21 +0500

    [ls-files] try first git instead of fd

--- ls-files.sh
+++ ls-files.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-if [ -x "$(command -v fd)" ]; then
+if [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    exec git ls-files
+elif [ -x "$(command -v fd)" ]; then
     exec fd --type file --follow --hidden --exclude .git --color never
 elif [ -x "$(command -v rg)" ]; then
     exec rg --files --color never --no-messages
-elif [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    exec git ls-files
 else
     exec ffind -type f 2> /dev/null
 fi
