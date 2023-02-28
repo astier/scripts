d265f70021fde209af15191b1bcf4e36cdf315c6
Aleksandrs Stier <aleks.stier@icloud.com>
2023-02-28 19:54:38 +0500

    [ls-dirs/-files] use fd if installed

--- ls-dirs.sh
+++ ls-dirs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-if [ -x "$(command -v rg)" ]; then
+if [ -x "$(command -v fd)" ]; then
+    exec fd --type directory --follow --hidden --exclude .git --color never
+elif [ -x "$(command -v rg)" ]; then
     exec rg --files --no-messages -0 | xargs -0 dirname | sed /^\.$/d | sort -u
 elif [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     exec git ls-files -z | xargs -0 dirname | sed /^\.$/d | sort -u
--- ls-files.sh
+++ ls-files.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-if [ -x "$(command -v rg)" ]; then
-    exec rg --files --no-messages
+if [ -x "$(command -v fd)" ]; then
+    exec fd --type file --follow --hidden --exclude .git --color never
+elif [ -x "$(command -v rg)" ]; then
+    exec rg --files --color never --no-messages
 elif [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     exec git ls-files
 else
