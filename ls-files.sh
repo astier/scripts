583b6505db00c7de7d79f04c91372d3d936578ee
Aleksandrs Stier <aleks.stier@icloud.com>
2023-03-06 14:40:04 +0500

    [ls-files] deduplicate git-output

--- ls-files.sh
+++ ls-files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    exec git ls-files
+    exec git ls-files --deduplicate
 elif [ -x "$(command -v fd)" ]; then
     exec fd --type file --follow --hidden --exclude .git --color never
 elif [ -x "$(command -v rg)" ]; then
