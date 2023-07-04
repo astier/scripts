6a6bdde997a56f36059ab72e163c7391699a15e4
Aleksandrs Stier <aleks.stier@icloud.com>
2023-07-04 10:31:05 +0200

    [rg] dont set --no-messages

--- ls-dirs.sh
+++ ls-dirs.sh
@@ -5,7 +5,7 @@ if [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 
 elif [ -x "$(command -v fd)" ]; then
     exec fd --type directory --hidden --exclude .git
 elif [ -x "$(command -v rg)" ]; then
-    exec rg --files --no-messages -0 | xargs -0 dirname | sed /^\.$/d | sort -u
+    exec rg --files -0 | xargs -0 dirname | sed /^\.$/d | sort -u
 else
     exec ffind -type d 2> /dev/null
 fi
--- ls-files.sh
+++ ls-files.sh
@@ -5,7 +5,7 @@ if [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 
 elif [ -x "$(command -v fd)" ]; then
     exec fd --type file --hidden --exclude .git
 elif [ -x "$(command -v rg)" ]; then
-    exec rg --files --no-messages
+    exec rg --files
 else
     exec ffind -type f 2> /dev/null
 fi
