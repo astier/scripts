9c106944249538def369b9204f3300bf0bd1d5fa
Aleksandrs Stier <aleks.stier@icloud.com>
2023-07-04 10:23:00 +0200

    [fd|rg] dont set '--color never'

--- ls-dirs.sh
+++ ls-dirs.sh
@@ -3,7 +3,7 @@
 if [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     exec git ls-files -z | xargs -0 dirname | sed /^\.$/d | sort -u
 elif [ -x "$(command -v fd)" ]; then
-    exec fd --type directory --hidden --exclude .git --color never
+    exec fd --type directory --hidden --exclude .git
 elif [ -x "$(command -v rg)" ]; then
     exec rg --files --no-messages -0 | xargs -0 dirname | sed /^\.$/d | sort -u
 else
--- ls-files.sh
+++ ls-files.sh
@@ -3,9 +3,9 @@
 if [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     exec git ls-files --deduplicate
 elif [ -x "$(command -v fd)" ]; then
-    exec fd --type file --hidden --exclude .git --color never
+    exec fd --type file --hidden --exclude .git
 elif [ -x "$(command -v rg)" ]; then
-    exec rg --files --color never --no-messages
+    exec rg --files --no-messages
 else
     exec ffind -type f 2> /dev/null
 fi
