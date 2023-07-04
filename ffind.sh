e216903528b3963a42f31d5845570c6448cb7add
Aleksandrs Stier <aleks.stier@icloud.com>
2023-07-04 13:11:27 +0200

    [ffind] ignore dotfiles

--- ffind.sh
+++ ffind.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 
 exec find . -mindepth 1 \
-    -name .ccls-cache -prune -o \
-    -name .git -prune -o \
-    -name .idea -prune -o \
+    -name '.*' -prune -o \
     -name __pycache__ -prune -o \
     "$@" -printf "%P\n"
