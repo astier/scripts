93b89b46400b04f607e6a607be6310fdf8c0c378
Aleksandrs Stier <aleks.stier@icloud.com>
2023-07-17 18:12:53 +0200

    [ls-dirs/files] check first for fd and rg

--- ls-dirs.sh
+++ ls-dirs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-if [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    exec git ls-files -z | xargs -0 dirname | sed /^\.$/d | sort -u
-elif [ -x "$(command -v fd)" ]; then
+if [ -x "$(command -v fd)" ]; then
     exec fd --type directory --hidden --exclude .git
 elif [ -x "$(command -v rg)" ]; then
     exec rg --files -0 | xargs -0 dirname | sed /^\.$/d | sort -u
+elif [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    exec git ls-files -z | xargs -0 dirname | sed /^\.$/d | sort -u
 else
     exec ffind -type d 2> /dev/null
 fi
--- ls-files.sh
+++ ls-files.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-if [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    exec git ls-files --deduplicate
-elif [ -x "$(command -v fd)" ]; then
+if [ -x "$(command -v fd)" ]; then
     exec fd --type file --hidden --exclude .git
 elif [ -x "$(command -v rg)" ]; then
     exec rg --files
+elif [ -x "$(command -v git)" ] && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    exec git ls-files --deduplicate
 else
     exec ffind -type f 2> /dev/null
 fi
