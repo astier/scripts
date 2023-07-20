9bead8dc4ea27325d7b8da7d28be637df6adde14
Aleksandrs Stier <aleks.stier@icloud.com>
2023-07-20 15:33:10 +0200

    [volume] use pulsemixer only for tui

--- volume.sh
+++ volume.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env sh
 
-if [ -x "$(command -v pulsemixer)" ]; then
-    case $1 in
-        -) pulsemixer --change-volume -4 ;;
-        +) pulsemixer --change-volume +4 ;;
-        =) pulsemixer --toggle-mute ;;
-        ?) pulsemixer --get-volume | cut -d' ' -f1 ;;
-        *) pulsemixer ;;
-    esac
-else
-    case $1 in
-        -) amixer set Master 2%- > /dev/null ;;
-        +) amixer set Master 2%+ > /dev/null ;;
-        =) amixer set Master toggle > /dev/null ;;
-        \?) amixer get Master | tail -n1 | awk -F'[][]' '{ print $2 }' | sed s/%// ;;
-        d) rm -f ~/.asoundrc ;;
-        h) echo defaults.pcm.device 3 > ~/.asoundrc ;;
-        "") alsamixer ;;
-    esac
-fi
+case $1 in
+  -) amixer set Master 2%- > /dev/null ;;
+  +) amixer set Master 2%+ > /dev/null ;;
+  =) amixer set Master toggle > /dev/null ;;
+  \?) amixer get Master | tail -n1 | awk -F'[][]' '{ print $2 }' | sed s/%// ;;
+  d) rm -f ~/.asoundrc ;;
+  h) echo defaults.pcm.device 3 > ~/.asoundrc ;;
+  "") pulsemixer 2> /dev/null || alsamixer ;;
+esac
